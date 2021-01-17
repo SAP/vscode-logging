069fb4fed71058225d471539f496297cee01f605
Shahar Soel <shachar.soel@sap.com>
2021-01-17 20:00:52 +0200

    chore: cr fixes

--- examples/extension-wrapper/src/extension.ts
+++ examples/extension-wrapper/src/extension.ts
@@ -1,44 +1,9 @@
-import { readFileSync } from "fs";
-import { resolve } from "path";
-import { ok } from "assert";
-import { ExtensionContext, window, workspace } from "vscode";
-import { configureLogger } from "@vscode-logging/wrapper";
-import { getLogger, setLogger } from "./logger";
+import { ExtensionContext } from "vscode";
+import { getLogger, initLogger } from "./logger";
 import { registerCommands } from "./commands";
-
-const LOGGING_LEVEL_PROP = "Example_Logging.loggingLevel";
-const SOURCE_LOCATION_PROP = "Example_Logging.sourceLocationTracking";
-
-function initLogger(context: ExtensionContext): void {
-  const meta = JSON.parse(
-    readFileSync(resolve(context.extensionPath, "package.json"), "utf8")
-  );
-
-  // By asserting the existence of the properties in the package.json
-  // at runtime, we avoid many copy-pasta mistakes...
-  const configProps = meta?.contributes?.configuration?.properties;
-  ok(configProps?.[LOGGING_LEVEL_PROP]);
-  ok(configProps?.[SOURCE_LOCATION_PROP]);
-
-  const extLogger = configureLogger({
-    extName: meta.displayName,
-    logPath: context.logPath,
-    logOutputChannel: window.createOutputChannel(meta.displayName),
-    // set to `true` if you also want your VSCode extension to log to the console.
-    logConsole: false,
-    loggingLevelProp: LOGGING_LEVEL_PROP,
-    sourceLocationProp: SOURCE_LOCATION_PROP,
-    subscriptions: context.subscriptions,
-    onDidChangeConfiguration: workspace.onDidChangeConfiguration,
-    getConfiguration: workspace.getConfiguration
-  });
-
-  setLogger(extLogger);
-
-  registerCommands(context.subscriptions);
-}
 
 export function activate(context: ExtensionContext): void {
   initLogger(context);
   getLogger().info("extension is active, hip hip hurray!");
+  registerCommands(context.subscriptions);
 }
--- examples/extension-wrapper/src/logger.ts
+++ examples/extension-wrapper/src/logger.ts
@@ -1,8 +1,12 @@
 /**
  * This file manages the logger's state.
  */
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import { ok } from "assert";
+import { ExtensionContext, window, workspace } from "vscode";
 import { IChildLogger, IVSCodeExtLogger } from "@vscode-logging/types";
-import { NOOP_LOGGER } from "@vscode-logging/wrapper";
+import { configureLogger, NOOP_LOGGER } from "@vscode-logging/wrapper";
 
 // On file load we initialize our logger to `NOOP_LOGGER`
 // this is done because the "real" logger cannot be initialized during file load.
@@ -14,6 +18,36 @@ export function getLogger(): IChildLogger {
   return loggerImpel;
 }
 
-export function setLogger(newLogger: IVSCodeExtLogger): void {
+function setLogger(newLogger: IVSCodeExtLogger): void {
   loggerImpel = newLogger;
+}
+
+const LOGGING_LEVEL_PROP = "Example_Logging.loggingLevel";
+const SOURCE_LOCATION_PROP = "Example_Logging.sourceLocationTracking";
+
+export function initLogger(context: ExtensionContext): void {
+  const meta = JSON.parse(
+    readFileSync(resolve(context.extensionPath, "package.json"), "utf8")
+  );
+
+  // By asserting the existence of the properties in the package.json
+  // at runtime, we avoid many copy-pasta mistakes...
+  const configProps = meta?.contributes?.configuration?.properties;
+  ok(configProps?.[LOGGING_LEVEL_PROP]);
+  ok(configProps?.[SOURCE_LOCATION_PROP]);
+
+  const extLogger = configureLogger({
+    extName: meta.displayName,
+    logPath: context.logPath,
+    logOutputChannel: window.createOutputChannel(meta.displayName),
+    // set to `true` if you also want your VSCode extension to log to the console.
+    logConsole: false,
+    loggingLevelProp: LOGGING_LEVEL_PROP,
+    sourceLocationProp: SOURCE_LOCATION_PROP,
+    subscriptions: context.subscriptions,
+    onDidChangeConfiguration: workspace.onDidChangeConfiguration,
+    getConfiguration: workspace.getConfiguration
+  });
+
+  setLogger(extLogger);
 }
